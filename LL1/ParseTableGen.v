4f7d90242143b6e60412c2bbf707cd627758b170
slasser <samuel.lasser@tufts.edu>
2018-07-30 19:17:49 -0400

    Unstable commit -- working on verification

--- LL1/ParseTableGen.v
+++ LL1/ParseTableGen.v
@@ -8,6 +8,16 @@ Require Import LL1.ParseTable.
 
 Import ListNotations.
 
+Definition pl_pair := (production * LaSet.t)%type.
+
+Definition pl_pair_wf (g : grammar) (p : pl_pair) :=
+  match p with
+  | ((x, gamma), laSet) => lookahead_set_for laSet x gamma g
+  end.
+
+Definition pl_pairs_wf (g : grammar) (ps : list pl_pair) :=
+  Forall (pl_pair_wf g) ps.
+
 Definition singletonLaMap la gamma :=
   LaMap.add la gamma (LaMap.empty (list symbol)).
   
@@ -26,12 +36,56 @@ Definition addEntry (x : nonterminal) (la : lookahead) (gamma : list symbol) (o 
     end
   end.
 
-Definition addProductionEntries (production_and_la_set : production * LaSet.t)
-                                (o : option parse_table) :=
-  match production_and_la_set with
+Definition addEntries (p : pl_pair) (o : option parse_table) :=
+  match p with
   | ((x, gamma), laSet) =>
     fold_right (fun la o => addEntry x la gamma o) o (LaSet.elements laSet)
   end.
+
+Definition tbl_sound_for_pl_pairs tbl pls :=
+  forall x la gamma,
+    pt_lookup x la tbl = Some gamma
+    -> exists laSet,
+      In ((x, gamma), laSet) pls
+      /\ LaSet.In la laSet.
+
+Definition tbl_complete_for_pl_pairs tbl pls :=
+  forall x gamma la laSet,
+    In ((x, gamma), laSet) pls
+    -> LaSet.In la laSet
+    -> pt_lookup x la tbl = Some gamma.
+
+Definition tbl_correct_for_pl_pairs tbl pls :=
+  tbl_sound_for_pl_pairs tbl pls /\ tbl_complete_for_pl_pairs tbl pls.
+
+Lemma addEntries_preserves_invariant :
+  forall laSet g p ps x gamma tbl tbl',
+    p = ((x, gamma), laSet)
+    -> pl_pairs_wf g (p :: ps)
+    -> tbl_correct_for_pl_pairs tbl ps 
+    -> addEntries p (Some tbl) = Some tbl'
+    -> tbl_correct_for_pl_pairs tbl' (p :: ps).
+Proof.
+  intros laSet.
+  remember (LaSet.elements laSet) as elts.
+  generalize dependent laSet.
+  induction elts; intros laSet Helts g p ps x gamma tbl tbl' Heq Htc Had.
+  - subst.
+    admit.
+  - unfold pl_pairs_wf in Htc. 
+    inv Htc.
+subst.
+    unfold addEntries in Had. 
+    rewrite <- Helts in Had.
+    simpl in Had.
+    destruct (fold_right (fun la o => addEntry x la gamma o) (Some tbl) elts) eqn:Htl.
+    + unfold addEntry in Had.
+      destruct (NtMap.find x p) eqn:Hnf.
+      * destruct (LaMap.find a t) eqn:Hlf.
+        -- destruct (list_eq_dec symbol_eq_dec gamma l).
+           ++ inv Had.
+              eapply IHelts; eauto.
+
 
 Definition empty_pt := NtMap.empty (LaMap.t (list symbol)).
 
