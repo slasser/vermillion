173331198042b4687f8631b81bd8850d99b2f5ba
Sam Lasser <samuel.lasser@tufts.edu>
2019-03-29 23:40:09 -0400

    Add top-level proofs to main module

--- Main.v
+++ Main.v
@@ -1,19 +1,19 @@
 Require Import Grammar.
 Require Import mkParseTable_correct.
-Require Import Parser_sound.
+Require Import Parser_complete.
 
 Module Main (Import G : Grammar.T).
 
-  Module Import GeneratorProofs := GeneratorProofsFn G.
-  Module Import ParserSoundness := ParserSoundnessFn G.
+  Module Import GeneratorAndProofs := GeneratorProofsFn G.
+  Module Import ParserAndProofs    := ParserProofsFn G.
 
   Definition parseTableOf (g : grammar) : option parse_table :=
-    let nu := mkNullableSet g in
+    let nu    := mkNullableSet g in
     let nu_pf := (mkNullableSet_correct g) in
-    let fi := mkFirstMap g nu in
+    let fi    := mkFirstMap g nu in
     let fi_pf := (mkFirstMap_correct g nu nu_pf) in
-    let fo := mkFollowMap g nu fi fi_pf in
-    let es := mkEntries nu fi fo g in
+    let fo    := mkFollowMap g nu fi fi_pf in
+    let es    := mkEntries nu fi fo g in
     mkParseTable es.
 
   Theorem parseTableOf_sound : 
@@ -46,16 +46,14 @@ Module Main (Import G : Grammar.T).
     - apply mkFollowMap_correct; auto.
       apply mkNullableSet_correct; auto.
   Qed.
-  
+
   Definition parse (tbl : parse_table)
-             (sym : symbol)
-             (input : list terminal) :
+                   (sym : symbol)
+                   (input : list terminal) :
     Datatypes.sum parse_failure (tree * list terminal) :=
-    match parse_nf tbl sym input NtSet.empty
-                   (triple_lt_wf (meas tbl input NtSet.empty (F_arg sym)))
-    with
+    match parse_nf tbl sym input NtSet.empty (triple_lt_wf _) with
     | inl failure => inl failure
-    | inr (tr, existT _ input' Hle) => inr (tr, input')
+    | inr (tr, existT _ input' _) => inr (tr, input')
     end.
 
   Theorem parser_sound :
@@ -70,12 +68,59 @@ Module Main (Import G : Grammar.T).
   Proof.
     intros.
     unfold parse in *.
-    destruct (parse_nf tbl sym (word ++ rem) NtSet.empty (triple_lt_wf (meas tbl (word ++ rem) NtSet.empty (F_arg sym)))) eqn:Hp.
-    - tc.
-    - destruct p.
-      destruct s.
+    match goal with
+    | H : match ?X with | _ => _ end = _ |- _ => destruct X eqn:Hp
+    end; tc.
+    destruct p.
+    destruct s.
+    inversion H0; subst.
+    eapply parse_sound; eauto.
+  Qed.
+    
+  Theorem parse_safe :
+    forall (g   : grammar)
+           (tbl : parse_table)
+           (sym : symbol)
+           (input : list terminal)
+           (tr : tree),
+      parse_table_correct tbl g
+      -> forall x vis input',
+        ~ parse tbl sym input = inl (LeftRec x vis input').
+  Proof.
+    unfold not; intros.
+    unfold parse in H0.
+    match goal with
+    | H : match ?X with | _ => _ end = _ |- _ => destruct X eqn:Hp
+    end.
+    - destruct p; tc.
       inversion H0; subst.
-      eapply parse_sound; eauto.  
-Qed.
+      eapply leftrec_conditions with (sa := F_arg sym) in Hp; eauto.
+      destruct Hp.
+      + destruct H1.
+        ND.fsetdec.
+      + destruct H1.
+        eapply LL1_parse_table_impl_no_left_recursion; eauto.
+    - step; step; tc.
+  Qed.
 
+  Theorem parse_complete :
+    forall g tbl sym word tr rem,
+      parse_table_correct tbl g
+      -> (@sym_derives_prefix g) sym word tr rem
+      -> parse tbl sym (word ++ rem) = inr (tr, rem).
+  Proof.
+    intros.
+    eapply parse_nf_complete_or_leftrec in H0; eauto.
+    destruct H0.
+    - exfalso.
+      destruct H0 as [x [vis [input' Hp]]].
+      eapply parse_safe; eauto.
+      unfold parse.
+      rewrite Hp; eauto.
+    - destruct H0 as [Hle Hp].
+      unfold parse.
+      rewrite Hp; auto.
+  Qed.
+  
 End Main.
+
--- Proofs/Determinism.v
+++ Proofs/Determinism.v
@@ -5,100 +5,38 @@ Require Import Grammar.
 Require Import Tactics.
 
 Require Import Parser.
-Require Import ParseTable.
 Require Import Lemmas.
 
-Lemma LL1_derivation_deterministic :
-  forall (tbl : parse_table)
-         (g : grammar),
-    parse_table_for tbl g
-    -> forall (sym : symbol)
-              (word rem : list string)
-              (tr : tree),
-      (@sym_derives_prefix g) sym word tr rem
-      -> forall (word' rem' : list string)
-                (tr' : tree),
-        (@sym_derives_prefix g) sym word' tr' rem'
-        -> word ++ rem = word' ++ rem'
-        -> word = word' /\ rem = rem' /\ tr = tr'.
-Proof.
-  intros tbl g Htbl sym word rem tr Hder.
-  induction Hder using sdp_mutual_ind with
-      
-      (P := fun sym word tr rem (pf : sym_derives_prefix sym word tr rem) =>
-              forall word' rem' tr',
-                sym_derives_prefix sym word' tr' rem'
-                -> word ++ rem = word' ++ rem'
-                -> word = word' /\ rem = rem' /\ tr = tr')
-      
-      (P0 := fun gamma word f rem (pf : gamma_derives_prefix gamma word f rem) =>
-               forall word' rem' f',
-                 gamma_derives_prefix gamma word' f' rem'
-                 -> word ++ rem = word' ++ rem'              
-                 -> word = word' /\ rem = rem' /\ f = f').
+Require Import Parser_complete.
+
+Module DetFn (Import G : Grammar.T).
+
+  Module Import ParserProofs := ParserProofsFn G.
   
-  - (* T case *)
-    intros word' rem' tr' Hsdp Happ.
-    inv Hsdp; inv Happ; auto.
-    
-  - (* NT case *)
-    intros word' rem' tr' Hsdp Happ.
-    inv Hsdp.
-    (* The two right-hand sides must be equal because the grammar is LL(1) *)
-    assert (gamma0 = gamma).
-    { destruct l as [Hfi | Hfo]; destruct H1 as [Hfi' | Hfo'].
-      - (* first-first case *)
-        rewrite <- Happ in Hfi'.
-        destruct Htbl as [Hmin Hcom].
-        assert (lookahead_for (peek (word ++ rem)) x gamma g) by (left; auto).
-        assert (lookahead_for (peek (word ++ rem)) x gamma0 g) by (left; auto).
-        apply Hcom in H; auto.
-        apply Hcom in H1; auto.
-        congruence.
-      - (* first-follow conflict *)
-        exfalso.
-        destruct Hfo'.
-        eapply no_first_follow_conflicts with (sym := NT x); eauto.
-        rewrite <- Happ.
-        eapply first_gamma_first_sym with
-            (la := peek (word ++ rem))
-            (gamma := gamma); eauto.
-      - (* first-follow conflict *)
-        exfalso.
-        destruct Hfo.
-        eapply no_first_follow_conflicts with (sym := NT x); eauto.
-        rewrite Happ.
-        eapply first_gamma_first_sym; eauto.
-      - (* follow-follow case *)
-        destruct Hfo as [Hnu Hfo]; destruct Hfo' as [Hnu' Hfo'].
-        destruct Htbl as [Hmin Hcom].
-        unfold pt_complete in Hcom.
-        rewrite <- Happ in Hfo'.
-        assert (Hlk : lookahead_for (peek (word ++ rem)) x gamma g)
-          by (right; auto).
-        assert (Hlk' : lookahead_for (peek (word ++ rem)) x gamma0 g)
-          by (right; auto).
-        apply Hcom in Hlk; auto.
-        apply Hcom in Hlk'; auto.
-        congruence. }
-    subst.
-    eapply IHHder in H2; eauto.
-    do 2 destruct H2; subst; auto.
-
-  - (* nil case *)
-    intros word' rem' f' Hgdp Happ.
-    inv Hgdp; auto.
-
-  - (* cons case *)
-    intros word' rem' f' Hgdp Happ.
-    inv Hgdp.
-    eapply IHHder in H1; eauto.
-    + destruct H1.
-      destruct H0; subst.
-      eapply IHHder0 in H5; eauto.
-      destruct H5.
-      destruct H1.
-      subst; auto.
-    + repeat rewrite app_assoc; auto.
+  Lemma LL1_derivation_deterministic :
+    forall (tbl : parse_table)
+           (g : grammar),
+      parse_table_correct tbl g
+      -> forall (sym : symbol)
+                (word rem : list terminal)
+                (tr : tree),
+        (@sym_derives_prefix g) sym word tr rem
+        -> forall (word' rem' : list terminal)
+                  (tr' : tree),
+          (@sym_derives_prefix g) sym word' tr' rem'
+          -> word ++ rem = word' ++ rem'
+          -> word = word' /\ rem = rem' /\ tr = tr'.
+Proof.
+  intros tbl g Htbl sym word  rem  tr  Hder
+         word' rem' tr' Hder' Heq.
+  eapply parse_nf_complete in Hder;  eauto.
+  rewrite Heq in Hder.
+  eapply parse_nf_complete in Hder'; eauto.
+  destruct Hder as [Hle Hp].
+  destruct Hder' as [Hle' Hp'].
+  rewrite Hp in Hp'.
+  inv Hp'.
+  apply app_inv_tail in Heq; subst; auto.
 Qed.
 
+End DetFn.
