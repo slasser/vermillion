6c05fedd87ba2888f05e959e47de1dba56a465c6
slasser <samuel.lasser@tufts.edu>
2019-06-26 17:13:23 -0400

    Start updating evaluation to include semantic actions

--- Evaluation/cmp_menhir_ocaml_parser_and_LL1_parser/Extraction.v
+++ Evaluation/cmp_menhir_ocaml_parser_and_LL1_parser/Extraction.v
@@ -9,34 +9,133 @@ Require Import Main.
 
 Open Scope string_scope.
 
+(*
+
+type value = [
+  | `Assoc of (string * value) list
+  | `Bool of bool
+  | `Float of float
+  | `Int of int
+  | `List of value list
+  | `Null
+  | `String of string
+  ]
+*)
+
+Inductive jvalue :=
+| JAssoc  : list (string * jvalue) -> jvalue
+| JBool   : bool -> jvalue
+| JFloat  : nat -> jvalue
+| JInt    : nat -> jvalue
+| JList   : list jvalue -> jvalue
+| JNull   : jvalue
+| JString : string -> jvalue.
+
 (* First, we provide the types of grammar symbols 
    and their decidable equalities. *)
-Module NatStringTypes <: SYMBOL_TYPES.
-    Definition terminal := string.
-    Definition nonterminal := nat.
-    Definition literal := string.
-    Definition t_eq_dec := string_dec.
-    Definition nt_eq_dec := Nat.eq_dec.
-End NatStringTypes.
+Module Json_Types <: SYMBOL_TYPES.
+  
+  Inductive terminal' :=
+  | T_int 
+  | T_float 
+  | T_string 
+  | T_true 
+  | T_false 
+  | T_null 
+  | T_left_brace 
+  | T_right_brace 
+  | T_left_brack 
+  | T_right_brack 
+  | T_colon 
+  | T_comma.
+  
+  Definition terminal := terminal'.
+  
+  Inductive nonterminal' :=
+  | N_value 
+  | N_pairs 
+  | N_pairs_tl 
+  | N_pair 
+  | N_elts 
+  | N_elts_tl.
+  
+  Definition nonterminal := nonterminal'.
 
-(* Next, we generate grammar definitions for those types. *)
-Module Export NatStringGrammar <: T.
-  Module SymTy := NatStringTypes.
-  Module Defs  := DefsFn SymTy.
-  Export SymTy.
-  Export Defs.
-End NatStringGrammar.
+  Lemma t_eq_dec : forall (t t' : terminal),
+      {t = t'} + {t <> t'}.
+  Proof. decide equality. Defined.
+  
+  Lemma nt_eq_dec : forall (nt nt' : nonterminal),
+      {nt = nt'} + {nt <> nt'}.
+  Proof. decide equality. Defined.
 
+  Definition show_t (a : terminal) : string :=
+    match a with
+    | T_int         => "Int"
+    | T_float       => "Float"
+    | T_string      => "String"
+    | T_true        => "True"
+    | T_false       => "False"
+    | T_null        => "Null"
+    | T_left_brace  => "{"
+    | T_right_brace => "}" 
+    | T_left_brack  => "["
+    | T_right_brack => "]"
+    | T_colon       => ":"
+    | T_comma       => ","
+    end.
 
-Definition value  := 0.
-Definition pairs  := 1.
-Definition pairs' := 2.
-Definition pair   := 3.
-Definition elts   := 4.
-Definition elts'  := 5.
+  Definition show_nt (x : nonterminal) : string :=
+    match x with
+    | N_value    => "value"
+    | N_pairs    => "pairs"
+    | N_pairs_tl => "pairs_tl"
+    | N_pair     => "pair"
+    | N_elts     => "elts"
+    | N_elts_tl  => "elts_tl"
+    end.
+  
+  Definition t_semty (a : terminal) : Type :=
+    match a with
+    | T_int         => nat
+    | T_float       => nat
+    | T_string      => string
+    | _             => unit
+    end.
+
+  Definition nt_semty (x : nonterminal) : Type :=
+    match x with
+    | N_value    => jvalue
+    | N_pairs    => list (string * jvalue)
+    | N_pairs_tl => list (string * jvalue)
+    | N_pair     => string * jvalue
+    | N_elts     => list jvalue
+    | N_elts_tl  => list jvalue
+    end.
+
+End Json_Types.
+
+(* Next, we generate grammar definitions for those types,
+   and we package the types and their accompanying defs
+   into a single module *)
+Module Export G <: Grammar.T.
+  Module Export SymTy := Json_Types.
+  Module Export Defs  := DefsFn SymTy.
+End G.
 
 (* Now we can define a grammar as a record 
    containing a start symbol and a list of productions. *)
+Definition jsonGrammar : grammar :=
+  {| start := N_value ;
+     prods := [existT action_ty
+                      (N_value, [T T_left_brack; NT N_pairs; T T_right_brack])
+                      (fun tup => match tup with
+                                  | (_, (prs, (_, _))) =>
+                                    JAssoc prs
+                                  end)
+              ]
+  |}.
+
 Definition g : grammar :=
   {| start := value
   ; prods :=
